0c503b510923d9202445e18f003e7c756a11624a
Dalton Wu <daltonwu92@gmail.com>
2018-10-06 23:50:41 -0400

    Added support for MtgFormat Enum names containing spaces. Added support for all current formats in MtgFormat enum

--- src/main/java/mtg/MtgFormat.java
+++ src/main/java/mtg/MtgFormat.java
@@ -2,7 +2,20 @@ package mtg;
 
 public enum MtgFormat {
     STANDARD,
-    MODERN;
+    MODERN,
+    PAUPER,
+    LEGACY,
+    VINTAGE,
+    PENNY_DREADFUL,
+    COMMANDER_1V1,
+    Commander,
+    BRAWL,
+    ARENA_STANDARD;
+
+
+    public static MtgFormat getEnum(String string) {
+        return valueOf(string.replace(" ", "_").toUpperCase());
+    }
 
     public String toString() {
         return this.name().toLowerCase();
